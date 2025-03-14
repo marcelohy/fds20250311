9c8b2fa44392c91a3fd740c03507e9b0bf1d733d
Otávio <131626854+otaviobquadros@users.noreply.github.com>
2025-03-13 21:21:38 -0300

    Presenca Otavio

--- presenca/src/App.java
+++ presenca/src/App.java
@@ -2,5 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Henrique Corrales Marchiori");
+        System.out.println("Otávio Biazetto Quadros");
+
     }
 }
