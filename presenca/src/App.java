ce931628ff27b83bbf86c3a75268084143d8a31a
Henrique Corrales Marchiori <85183717+HMarchiori@users.noreply.github.com>
2025-03-13 21:07:35 -0300

    Update App.java

--- presenca/src/App.java
+++ presenca/src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        // Programe uma nova linha abaixo que mostre o seu nome completo
+        System.out.println("Henrique Corrales Marchiori");
     }
 }
