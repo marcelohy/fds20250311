35e554aa15f85ccfc4a87ec2fd6f9150eca925e8
Gabriel Botega <gabicbotega@gmail.com>
2025-03-13 21:10:00 -0300

    my name

--- presenca/src/App.java
+++ presenca/src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         // Programe uma nova linha abaixo que mostre o seu nome completo
+        System.out.println("Gabriel Caliari Botega");
     }
 }
