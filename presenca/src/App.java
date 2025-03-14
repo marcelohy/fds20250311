d73e24dd46ac1f9a1820e89334b71d44c1ce1dad
brunoroese <bruno.roese@functionize.com>
2025-03-13 21:11:27 -0300

    ignore .idea

--- presenca/src/App.java
+++ presenca/src/App.java
@@ -2,6 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
 
-        System.out.println("Bruno Roese");
+        System.out.println("Bruno Beckmann Roese");
     }
 }
