ddb858223ec3dcd60ba1c5299dfb831ed4374be8
davi kolodzeiczak iasculski <114928998+daviasculski@users.noreply.github.com>
2025-03-13 21:11:00 -0300

    Update App.java -- Davi Iasculski
    Adicionei um print com a string nome, que possui como valor meu nome.

--- presenca/src/App.java
+++ presenca/src/App.java
@@ -2,5 +2,10 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         // Programe uma nova linha abaixo que mostre o seu nome completo
+        
+        String nome = "Davi Kolodzeiczak Iasculski";
+        System.out.println(nome);
+
+        //Fork Davi Kolodzeiczak Iasculski
     }
 }
