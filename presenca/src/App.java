4f65e7be09a8267c604aa03cb77698f4474a885f
caihan90 <caihan.garcia@edu.pucrs.br>
2025-03-13 21:16:25 -0300

    Update App.java

--- presenca/src/App.java
+++ presenca/src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        System.out.println("Henrique Corrales Marchiori");
+        System.out.println("Caihan Araujo Garcia");
     }
 }
